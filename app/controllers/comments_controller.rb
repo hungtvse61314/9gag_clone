87140b6ccd207e7e929d5c749149da033aa45b4c
Viet Hung <hungtvse61314@gmail.com>
2017-04-19 21:39:15 +0700

    fix post

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,7 +4,7 @@ class CommentsController < ApplicationController
   end
   def create
     @post = Post.find(params[:ids])
-    @comment = @post.comments.create(params[:comment].permit(:body))
+    @comment = @post.comments.create(params[:comment].permit(:body).merge(user_id: current_user.id,post_id:current_post.id))
 
     redirect_to root_path(@post)
   end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,21 +1,30 @@
-class PostsController < ApplicationController
-  def index
+  class PostsController < ApplicationController
+    before_filter :authenticate_user!, only: [:new, :create]
+    before_action :set_post, only: [:show, :edit, :update, :destroy]
+    def index
     @posts = Post.all.order('Created_at DESC')
 
-  end
-  def new
+    end
+    def new
 
-  end
+    end
 
-  def create
-    @post = Post.new(params.require(:post).permit(:title,:image))
-    @post.save
+    def create
+      @post = Post.new(post_params)
+      @post.save
 
-    redirect_to @post
+      redirect_to @post
 
-  end
-  def show
-    @post = Post.find(params[:id])
+    end
+    def show
+      @post = Post.find(params[:id])
     #redirect_to root_path
+    end
+
+    def set_post
+      @post = Post.find(params[:id])
+    end
+  def post_params
+    params.require(:post).permit(:title,:image).merge(user_id: current_user.id)
   end
 end
