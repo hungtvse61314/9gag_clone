4237937b2d565f1e1be680059eba5a991e234eeb
Viet Hung <hungtvse61314@gmail.com>
2017-04-19 21:50:45 +0700

    a

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,7 +5,7 @@ class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:ids])
     @comment = @post.comments.create(params[:comment].permit(:body).merge(user_id: current_user.id,post_id:current_post.id))
-    
+
     redirect_to root_path(@post)
   end
 end
