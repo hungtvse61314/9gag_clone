97780e3db9b0570eeb477e64243f05aa09f363e4
Viet Hung <hungtvse61314@gmail.com>
2017-04-19 21:49:35 +0700

    fix conflict

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@ sao# This file is auto-generated from the current state of the database. Instead
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170419110638) do
+ActiveRecord::Schema.define(version: 20170419144128) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20170419110638) do
     t.integer  "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "author"
     t.index ["post_id"], name: "index_comments_on_post_id", using: :btree
     t.index ["user_id"], name: "index_comments_on_user_id", using: :btree
   end
